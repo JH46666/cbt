529b2fcaa26250b1f97ebe346baff0bcb90a979a
haoyifan <haoyifan@.zuipin.cn>
2017-11-15 14:29:05 +0800

    11

--- src_wap/views/goods/detail.vue
+++ src_wap/views/goods/detail.vue
@@ -6,14 +6,31 @@
             </div>
         </div>
         <div class="detail_footer">
-
+            <mt-tabbar v-model="selected" class="cbt-footer">
+                <mt-tab-item id="客服">
+                    <i class="icon-rexian" slot="icon"></i>
+                    客服
+                </mt-tab-item>
+                <mt-tab-item id="购物车">
+                    <i class="icon-shopcar" slot="icon"></i>
+                    购物车
+                    <mt-badge type="error" size="small">99+</mt-badge>
+                </mt-tab-item>
+                <mt-tab-item id="加入购物车">
+                    加入购物车
+                </mt-tab-item>
+            </mt-tabbar>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            selected: null
+        }
+    }
 }
 </script>
 
