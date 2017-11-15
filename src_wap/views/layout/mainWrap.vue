8f632f90cdc71d54ffa386d9568a5190b3dd57f5
fangliying <fangliying@zuipin.cn>
2017-11-15 12:01:15 +0800

    基础布局

--- src_wap/views/layout/mainWrap.vue
+++ src_wap/views/layout/mainWrap.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="container">
         <cbt-header v-if="!$tool.isWx()"></cbt-header>
-        <router-view class="content"></router-view>
+        <router-view class="content" :class="{'no-footer':$route.meta.hideFooter}"></router-view>
         <cbt-Footer v-if="!$route.meta.hideFooter"></cbt-Footer>
     </div>
 </template>
