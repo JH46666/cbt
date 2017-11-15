df68b0fc316f188dc057a7bcdc842db879051700
fangliying <fangliying@zuipin.cn>
2017-11-15 15:13:49 +0800

    地址详情

---
+++ src_wap/views/center/integral.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="integral">
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="less" scoped>
+    @import url('~@/styles/center/integral.less');
+</style>
