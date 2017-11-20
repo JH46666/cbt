31cb8b654a5f853361ad3eae3ac7446f19dc3dfd
fangliying <fangliying@zuipin.cn>
2017-11-20 18:32:00 +0800

    结算中心

---
+++ src_wap/views/balance/index.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="balance">
+        <div class="address">
+            <div class="left"><i class="icon-dizhi"></i></div>
+            <div class="center">
+                <p class="user">陈伟欸 &nbsp;&nbsp; 18006062601</p>
+                <p>很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址很长很长的地址</p>
+            </div>
+            <div class="right"><i class="icon-icon07"></i></div>
+        </div>
+
+        <section class="goods-pannel">
+            <div class="title">
+                <img src="../../assets/images/logo.png" alt="">
+                自营
+            </div>
+            <div class="goods-item">
+                <goods-img style="width:1.6rem;height:1.6rem;"></goods-img>
+                <div class="right">
+                    <p class="goods-title">醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型</p>
+                    <p class="goods-bd">
+                        <span class="price">￥500</span>
+                        <span class="num">×1</span>
+                    </p>
+                </div>
+            </div>
+            <div class="pro_free_caption">
+                <span class="full_free">满赠</span>
+                <span>满358元送金骏眉</span>
+            </div>
+            <div class="goods-item">
+                <goods-img style="width:1.6rem;height:1.6rem;"></goods-img>
+                <div class="right">
+                    <p class="goods-title">醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型醉品朴茶 安溪铁观音2017秋茶 乌龙茶 清香型</p>
+                    <p class="goods-bd">
+                        <span class="price">￥0.00</span>
+                        <span class="num">×1</span>
+                    </p>
+                </div>
+            </div>
+            <div class="sum">
+                商品总额: <span class="gold">1314.00 + 111积分</span>
+            </div>
+        </section>
+        <section class="goods-pannel">
+            <div class="title">
+                <i class="icon-dianpu"></i>
+                这是一家第三发地铺
+            </div>
+            <div class="goods-item">
+                <goods-img style="width:1.6rem;height:1.6rem;"></goods-img>
+                <div class="right">
+                    <p class="goods-title">醉品朴茶 安溪铁观音2017</p>
+                    <p class="goods-bd">
+                        <span class="price">￥500</span>
+                        <span class="num">×1</span>
+                    </p>
+                </div>
+            </div>
+            <div class="sum">
+                商品总额: <span class="gold">1314.00 + 111积分</span>
+            </div>
+        </section>
+        <section class="save-order">
+            <p>应付：￥1813.00</p>
+        </section>
+    </div>
+</template>
+
+<script>
+    import addressList from '../center/address.vue'
+    export default{
+
+    }
+</script>
+
+
+<style lang="less" scoped>
+    @import url('~@/styles/balance/index.less');
+</style>
--- src_wap/views/balance/payView.vue
+++ src_wap/views/balance/payView.vue
@@ -1,22 +1,36 @@
 <template>
-    <div id="balance-fail">
+    <div id="pay-view">
         <div class="hd">
             <!-- 失败 -->
-            <!-- <div class="fail-text">
-                <i class="icon-zhifushibai"></i>支付失败
-            </div>
-            <div class="fail-btn-wrap">
-                <mt-button type="default">查看订单</mt-button>
-                <mt-button type="default">立即支付</mt-button>
-            </div> -->
+            <template v-if="$route.name === '结算失败'">
+                <div class="fail-text">
+                    <i class="icon-zhifushibai"></i>支付失败
+                </div>
+                <div class="fail-btn-wrap">
+                    <mt-button type="default">查看订单</mt-button>
+                    <mt-button type="default">立即支付</mt-button>
+                </div>
+            </template>
             <!-- 成功 -->
-            <div class="success-text">
-                <i class="icon-zhifuchenggong"></i>支付成功
-            </div>
-            <div class="success-btn-wrap">
-                <mt-button type="default">查看订单</mt-button>
-                <mt-button type="default">返回首页</mt-button>
-            </div>
+            <template v-if="$route.name === '结算成功'">
+                <div class="success-text">
+                    <i class="icon-zhifuchenggong"></i>支付成功
+                </div>
+                <div class="success-btn-wrap">
+                    <mt-button type="default">查看订单</mt-button>
+                    <mt-button type="default">返回首页</mt-button>
+                </div>
+            </template>
+            <!-- 货到付款 -->
+            <template v-if="$route.name === '货到付款结算'">
+                <div class="delivery-text">
+                    <i class="icon-zhifuchenggong"></i>货到付款订单提交成功
+                </div>
+                <div class="delivery-btn-wrap">
+                    <mt-button type="default">查看订单</mt-button>
+                    <mt-button type="default">立即支付</mt-button>
+                </div>
+            </template>
         </div>
         <div class="bd">
             <div class="text-item">
@@ -35,13 +49,20 @@
                 <div class="right">在线支付</div>
             </div>
         </div>
-        <div class="you-like">
-            
-        </div>
+        <!-- 猜你喜欢 -->
+        <may-like v-if="$route.name === '结算成功'"></may-like>
     </div>
 </template>
 
-
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
 
 
 <style lang="less" scoped>
