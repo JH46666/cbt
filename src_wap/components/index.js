e3c768b1f8e51bbf1bd55fbe1caf303edf807c5b
liuting <liuting@zuipin.cn>
2017-11-20 15:38:37 +0800

    dd

--- src_wap/components/index.js
+++ src_wap/components/index.js
@@ -2,6 +2,7 @@ import searchBar from './searchBar.vue';
 import plusOreduce from './plusOreduce.vue'
 import goodsItem from './goodsItem.vue';
 import goodsImg from './goodsImg.vue';
+import mayLike from './mayLike.vue';
 
 export default {
     install(Vue,option) {
@@ -9,5 +10,6 @@ export default {
         Vue.component('plusOreduce',plusOreduce);
         Vue.component(goodsImg.name,goodsImg);
         Vue.component(goodsItem.name,goodsItem);
+        Vue.component(mayLike.name,mayLike);
     }
 }
