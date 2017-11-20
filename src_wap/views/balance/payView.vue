3ff995dddbf3a4651f634843e1560673934459b1
fangliying <fangliying@zuipin.cn>
2017-11-20 16:06:12 +0800

    订单状态

---
+++ src_wap/views/balance/payView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="balance-fail">
+        <div class="hd">
+            <!-- 失败 -->
+            <!-- <div class="fail-text">
+                <i class="icon-zhifushibai"></i>支付失败
+            </div>
+            <div class="fail-btn-wrap">
+                <mt-button type="default">查看订单</mt-button>
+                <mt-button type="default">立即支付</mt-button>
+            </div> -->
+            <!-- 成功 -->
+            <div class="success-text">
+                <i class="icon-zhifuchenggong"></i>支付成功
+            </div>
+            <div class="success-btn-wrap">
+                <mt-button type="default">查看订单</mt-button>
+                <mt-button type="default">返回首页</mt-button>
+            </div>
+        </div>
+        <div class="bd">
+            <div class="text-item">
+                <div class="left">陈达团子</div>
+                <div class="right">18606062601</div>
+            </div>
+            <div class="text-item">
+                <p>公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面公交总站对面</p>
+            </div>
+            <div class="text-item">
+                <div class="left">待付金额：</div>
+                <div class="right"><span class="gold">￥5000</span></div>
+            </div>
+            <div class="text-item">
+                <div class="left">支付方式：</div>
+                <div class="right">在线支付</div>
+            </div>
+        </div>
+        <div class="you-like">
+            
+        </div>
+    </div>
+</template>
+
+
+
+
+<style lang="less" scoped>
+    @import url('~@/styles/balance/payView.less');
+</style>
