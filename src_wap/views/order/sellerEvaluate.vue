7b8fa44fe7e9cab7747c7f4f1d5aa8c0d7e8c22f
haoyifan <haoyifan@.zuipin.cn>
2017-11-21 16:29:25 +0800

    评论

---
+++ src_wap/views/order/sellerEvaluate.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <div class="evaluate">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+@import '~@/styles/order/evaluate.less';
+</style>
