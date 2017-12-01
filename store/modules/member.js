54ce09153331ed3d942d7ad25f54d2219ee69548
fangliying <fangliying@zuipin.cn>
2017-12-01 16:08:19 +0800

    红包列表

--- store/modules/member.js
+++ store/modules/member.js
@@ -10,7 +10,10 @@ const state = {
         total: 0,
         UNUSED: [],
         USED: [],
-        EXPIRED: []
+        EXPIRED: [],
+        UNUSEDNUM: 0,
+        USEDNUM: 0,
+        EXPIREDNUM: 0 
     }
 }
 
@@ -19,6 +22,7 @@ const mutations = {
         state[obj.type] = obj.val;
     },
     SET_REDPACKET(state,obj) {
+        if(obj.val === null) obj.val = [];
         state.redpacket[obj.type] = obj.val;
     }
 }
@@ -31,28 +35,53 @@ const actions = {
                 for (let attr in res.data) {
                     this.commit('SET_MEMBERDATA',{type:attr,val:res.data[attr]})
                 }
-                this.dispatch('viewSign')
-                this.dispatch('getRedTotal')
                 resolve(res.data);
             })
         })
     },
     // 查看会员是否签到
-    viewSign() {
+    viewSign({state,commit}) {
         $api.get('/member/memberRecord/searchIsSignIn',{
-            memberId: this.state.member.member.id,
+            memberId: state.member.id,
             sysId: 1
         },res => {
-            this.commit('SET_MEMBERDATA',{type:'sign',val:res.data})
+            commit('SET_MEMBERDATA',{type:'sign',val:res.data})
         })
     },
     // 获取红包个数
-    getRedTotal() {
+    getRedTotal({state,commit}) {
         $api.get('/member/redPacket/countRedPack',{
-            memberId: this.state.member.member.id,
+            memberId: state.member.id,
             sysId: 1
         },res => {
-            this.commit('SET_REDPACKET',{type:'total',val:res.data.total})
+            commit('SET_REDPACKET',{type:'total',val:res.data.total})
+        })
+    },
+    // 获取红包列表
+    getRedList({state,commit,dispatch},obj) {
+
+        let getData = (resolve,reject) => {
+            obj.id = state.member.id;
+            $api.get('/member/redPacket/searchRedPacketByStatus',{
+                'page.pageNumber': obj.page || 1,
+                'page.pageSize': 10,
+                'memberId': obj.id,
+                'status': obj.type,
+                'sysId': 1
+            },res => {
+                resolve(res)
+            })
+        }
+
+        return new Promise((resolve,reject) => {
+            // 刷新后重新获取会员
+            if(state.member.id) {
+                getData(resolve,reject)
+            } else {
+                dispatch('getMemberData').then(res => {
+                    getData(resolve,reject);
+                })
+            }
         })
     }
 }
