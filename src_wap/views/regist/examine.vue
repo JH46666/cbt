4e7a1825cc7f26f7d904f2b946e6cf17e5351d99
fangliying <fangliying@zuipin.cn>
2017-12-14 19:32:19 +0800

    店铺审核展示页

--- src_wap/views/regist/examine.vue
+++ src_wap/views/regist/examine.vue
@@ -3,13 +3,8 @@
         <div class="t_txt"><p class="color_9">恭喜您成功注册，请填写以下资料等待审核，审核后才可以询价和下单，如需帮助，请拨打 400-996-3399</p></div>
         <div class="seller_content">
             <div class="shop_img">
-                <div class="camera_box">
-                    <img src="../../assets/images/camera_cirle.png" alt="">
-                    <p>上传门店照片</p>
-                </div>
                 <div class="uploaded_box">
-                    <img class="shop" src="../../assets/images/shop_img.jpg" alt="">
-                    <a class="small_camera" href="javascript:void(0);"><img src="../../assets/images/camera.png" alt=""></a>
+                    <img class="shop" :src="orgDTO.facadePics" alt="">
                 </div>
             </div>
             <div class="shop_info">
@@ -17,32 +12,27 @@
                     <div class="form_item">
                         <div class="flex align_items_end">
                             <label for="">登录账号</label>
-                            <p v-if="!edit" class="flex-1">18720989159</p>
-                            <input v-else class="flex-1" type="text" name="" value="18720989159" disabled>
+                            <p  class="flex-1">{{ member.phone }}</p>
                         </div>
                         <p  class="error_txt"></p>
                     </div>
                     <div class="form_item">
-                        <div class="flex align_items_end error_color">
+                        <div class="flex align_items_end">
                             <label for="">店铺名称</label>
-                            <p v-if="!edit" class="flex-1">茶叶店</p>
-                            <input v-else class="flex-1" type="text" name="" value="茶叶店" placeholder="请填写店铺名称">
+                            <p  class="flex-1">{{ orgDTO.orgName }}</p>
                         </div>
-                        <p  class="error_txt">你错了你错了</p>
                     </div>
                     <div class="form_item">
                         <div class="flex align_items_end">
                             <label for="">联系人</label>
-                            <p v-if="!edit" class="flex-1">陈喂喂</p>
-                            <input v-else class="flex-1" type="text" name="" value="陈喂喂" placeholder="请填写联系人姓名">
+                            <p  class="flex-1">{{ orgDTO.contactPerson }}</p>
                         </div>
                         <p  class="error_txt"></p>
                     </div>
                     <div class="form_item">
                         <div class="flex align_items_end">
                             <label for="">地区</label>
-                            <p v-if="!edit" class="flex-1">福建/厦门/思明区</p>
-                            <input v-else class="flex-1" type="text" name="" value="福建/厦门/思明区" placeholder="请选择地区">
+                            <p  class="flex-1">{{ orgDTO.provinceName }}/{{ orgDTO.cityName }}/{{ orgDTO.countyName }}</p>
                             <!-- <a href="javascript:void(0);"><i class="iconfont">&#xe619;</i></a> -->
                         </div>
                         <p  class="error_txt"></p>
@@ -50,28 +40,51 @@
                     <div class="form_item">
                         <div class="flex align_items_end">
                             <label for="">店铺地址</label>
-                            <p v-if="!edit" class="flex-1">江苏省苏州市姑苏区金阊新城虎殿路1999号中海御景湾25-3108</p>
-                            <input v-else class="flex-1" type="text" name="" value="江苏省苏州市姑苏区金阊新城虎殿路1999号中海御景湾25-3108" placeholder="请填写详细店铺地址">
+                            <p  class="flex-1">{{ orgDTO.address }}</p>
                         </div>
                         <p class="error_txt"></p>
                     </div>
                 </form>
-                <div class="examine_img"><img src="../../assets/images/examine.png" alt=""></div>
+                <div class="examine_img" v-if="memberAccount.status === 'WAIT_AUDIT'"><img src="../../assets/images/examine.png" alt=""></div>
+                <div class="examine_img" v-else><img src="../../assets/images/shbtg.png" alt=""></div>
             </div>
-            <mt-button size="large" v-if="!edit" @click="edit = true" type="primary">修改资料</mt-button>
-            <mt-button size="large" v-else @click="edit = false" type="primary">确定</mt-button>
-            <div class="go_index"><a href="javascript:void(0);">去首页</a></div>
+            <mt-button size="large"  @click="$router.push({name: '茶帮通注册2'})">修改资料</mt-button>
+            <div class="go_index"><a href="javascript:void(0);" @click="$router.push('/')">去首页</a></div>
         </div>
     </div>
 </template>
 <script>
+    import { mapState } from 'vuex'
+    import store from 'store';
     export default {
         data(){
             return {
                edit: false
             }
         },
+        computed: {
+            ...mapState({
+                member: state => state.member.member,
+                memberAccount: state => state.member.memberAccount,
+                orgDTO: state => state.member.orgDTO,
+                shop: state => state.member.shop,
+                thirdAccount: state => state.member.thirdAccount,
+            })
+        },
         methods:{
+
+        },
+        // 进来先判断登陆与否
+        beforeRouteEnter(to, from, next) {
+            if(!store.state.member.member.id) {
+                store.dispatch('getMemberData').then((res) => {
+                    next();
+                }).catch(res => {
+                    next(vm => vm.$router.push('/login'));
+                })
+            } else {
+                next();
+            }
         }
 
     }
