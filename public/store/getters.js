612c6e1178a2b862bb0206a45f54b072ce62ee9d
fangliying <fangliying@zuipin.cn>
2017-12-14 18:04:26 +0800

    删除public

--- public/store/getters.js
+++
@@ -1,8 +0,0 @@
-
-const getters = {
-    orderStatus: state => state.order.status,
-    cartData: state => state.cart.cartData,
-    resize: state => state.create.resize,
-}
-
-export default getters
