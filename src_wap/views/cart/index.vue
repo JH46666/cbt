321293284c788b8eca8c6566c4538d7cf4b721f8
fangliying <fangliying@zuipin.cn>
2017-12-16 16:05:40 +0800

    修复弹窗bug

--- src_wap/views/cart/index.vue
+++ src_wap/views/cart/index.vue
@@ -558,9 +558,7 @@
                         message:`您的账号因违规操作而被冻结无法买买买~若有疑问，请联系客服400-996-3399`,
                         confirmButtonText: '我知道了'
                     }).then(res => {
-                        vm.$api.get('/oteao/login/logout',{},res => {
-                            document.querySelector('.mint-msgbox-wrapper').remove();
-                        })
+                        vm.$api.get('/oteao/login/logout',{},res => {})
                     })
                 }
                 if(status === 'INACTIVE') {
