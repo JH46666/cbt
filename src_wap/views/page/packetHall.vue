66dfde64bec219122a1c662dcc4eb32caf11f560
haoyifan <haoyifan@.zuipin.cn>
2017-12-28 19:28:35 +0800

    11

---
+++ src_wap/views/page/packetHall.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div class="packet">
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+@import '~@/styles/base/tools.less';
+.packet{
+
+}
+</style>
