8bb544f5879138f6dd1e3edfe42420c398e76c17
haoyifan <haoyifan@.zuipin.cn>
2018-01-02 18:31:43 +0800

    11

--- src_wap/views/page/packetHall.vue
+++ src_wap/views/page/packetHall.vue
@@ -8,7 +8,7 @@
             <div class="packet_user_wrapper">
                 <div class="packet_user_logo"></div>
                 <div class="packet_user_text">
-                    <div class="swiper-container-1">
+                    <div class="swiper-container-2">
                         <div class="swiper-wrapper">
                             <div class="swiper-slide">
                                 111111111111111111111111111
@@ -179,6 +179,14 @@ export default {
         showNewList() {
 
         }
+    },
+    mounted() {
+        var swiper2 = new Swiper('.swiper-container-2', {
+            direction: 'vertical',
+            autoplay: true,
+            loop: true,
+            slidesPerView :2 
+        });
     },
     methods: {
         // 签到
