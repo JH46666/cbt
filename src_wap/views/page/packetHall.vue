36aa23a1aeff6f25e6ae5eccd62c16838bc83962
fangliying <fangliying@zuipin.cn>
2018-01-02 19:27:55 +0800

    1

--- src_wap/views/page/packetHall.vue
+++ src_wap/views/page/packetHall.vue
@@ -105,7 +105,7 @@
                             <div class="packet_old_item_head">
                                 <!-- <div class="packet_logo"><img src="../../assets/images/ic_hbdsf.png" /></div> -->
                                 <div class="packet_logo"><img src="../../assets/images/ic_hbzy.png" /></div>
-                                <div class="packet_price">
+                                <div class="packet_price new">
                                     <div class="packet_sum align_left">
                                         <template v-if="item.amount">
                                             <span>￥</span>
@@ -253,7 +253,7 @@ export default {
                         this.newUserDialog = true;
                     })
                 },res => {
-                    
+                    this.$toast(res.message);
                 })
             }
 
