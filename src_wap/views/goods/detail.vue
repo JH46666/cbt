c1d392392b17e88185f7cdcd465732f31886e4cd
liuting <liuting@zuipin.cn>
2018-02-28 09:10:13 +0800

    layim

--- src_wap/views/goods/detail.vue
+++ src_wap/views/goods/detail.vue
@@ -819,7 +819,168 @@ export default {
            })
        },
        openKfDialog() {
-           this.showOrHide = true;
+        //    this.showOrHide = true;
+            let kefuId = 1;
+            let kefuName = "茶帮通客服";
+            if(detailData.productInfo.orgId){
+                kefuId = detailData.productInfo.orgId;
+                kefuName = detailData.orgShopCenterVo.shopName;
+            }
+            let data = {username: store.state.member.member.id,password: store.state.member.member.id};
+            let ret = '';
+            for (let it in data) {
+                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&';
+            }
+            this.$http.post("/erp/account/login",ret).then(res=>{
+                layui.config({
+                    version: true,
+                    base: '/static/mods/'
+                }).use(['mobile','socket','req'], function(mobile,socket,req){
+                    var layim = mobile.layim,
+                        layer = mobile.layer;
+                    var $ =layui.jquery;
+                    var selfFlag = false; 
+                    //基础配置
+                    layim.config({
+                        init: {
+                        //设置我的基础信息
+                        mine: {
+                            "username": store.state.member.member.nickName //我的昵称
+                            ,"id": store.state.member.member.id //我的ID
+                            ,"avatar": "http://tp4.sinaimg.cn/1345566427/180/5730976522/0" //我的头像
+                        }
+                        //好友列表数据
+                        ,friend: [] //见下文：init数据格式
+                        }
+                    });
+                    //创建一个会话
+                    layim.chat({
+                        id: kefuId
+                        ,name: kefuName
+                        ,type: 'kefu' //friend、group等字符，如果是group，则创建的是群聊
+                        ,avatar: 'http://tp1.sinaimg.cn/1571889140/180/40030060651/1'
+                    });
+                    socket.config({
+                        log:true,
+                        // token:'/erp/layim/getToKenById?id=204736',
+                        token:'/erp/layim/token',
+                        server:'ws://192.168.7.212:8888'
+                    });
+    
+                    socket.on('open',function (e) {
+                        console.log("监听到事件：open");
+                    });
+                    socket.on('close',function (e) {
+                        console.log("监听到事件：close");
+                    });
+                    socket.on('error',function (e) {
+                        console.log("监听到事件：error");
+                    });
+                    socket.on('msg',function (e) {
+                        var msg = JSON.parse(e.data);
+                        console.log(msg);
+                        var handleChat = function (msg) {
+                            if (selfFlag) {
+                                selfFlag = false;
+                                return;
+                            }
+                            layim.getMessage(msg);
+                        }
+                        var handleStatus=function (msg) {
+                            var status ='';
+                            if(typeof msg.count !== 'undefined'){
+                                status = msg.count+'人在线';
+                            }else{
+                                status = msg.online?'在线':'离线';
+                            }
+                            layim.setChatStatus('<span style="color:#FF5722;">'+status+'</span>');
+                        }
+                        if (msg.mtype) {
+                            switch (msg.mtype) {
+                                case -1:
+                                    return console.log(msg.msg);
+                                case socket.mtype.chatFriend:
+                                case socket.mtype.chatGroup:
+                                    handleChat(msg);
+                                    break;
+                                case socket.mtype.checkIsOnline:
+                                case socket.mtype.checkOnlineCount:
+                                    handleStatus(msg);
+                                    break;
+                                case socket.mtype.serverNotice:
+                                    layim.msgbox(msg.count);
+                                    break;
+                                case socket.mtype.addFriendNotice:
+                                    layim.addList({
+                                        type:msg.type,
+                                        avatar:msg.avatar,
+                                        username:msg.username,
+                                        groupid:msg.groupid,
+                                        sign:msg.sign,
+                                        id:msg.id
+                                    });
+                                    msg.mtype=socket.mtype.chatFriend;
+                                    msg.content="我们已经是好友啦，一起来聊天吧！";
+                                    layim.getMessage(msg);
+                                    break;
+                                case socket.mtype.onofflineNotice:
+                                    layim.setFriendStatus(msg.id,msg.status);
+                                    break;
+                            }
+                        }
+                    });
+                    // console.log(socket.mtype);
+                    //监听在线状态的切换事件
+                    layim.on('online', function(data){
+                        //console.log(data);
+                    });
+                    //监听签名修改
+                    layim.on('sign', function(value){
+                        //console.log(value);
+                    });
+                    //监听自定义工具栏点击，以添加代码为例
+                    layim.on('tool(code)', function(insert){
+                        layer.prompt({
+                            title: '插入代码'
+                            ,formType: 2
+                            ,shade: 0
+                        }, function(text, index){
+                            layer.close(index);
+                            insert('[pre class=layui-code]' + text + '[/pre]'); //将内容插入到编辑器
+                        });
+                    });
+                    //监听layim建立就绪
+                    layim.on('ready', function(){
+                        req.loading = false;
+                        req.get('/layim/apply-unread',{},function (res) {
+                            res.data&&layim.msgbox(res.data);
+                        });
+                    console.log(layim.cache().friend);
+                    });
+                    //监听发送消息
+                    layim.on('sendMessage', function(data){
+                        var To = data.to;
+                        var t = data.to.type=='friend';
+                        if(!t){
+                            selfFlag = true;
+                        }
+                        socket.send({mtype:(t?socket.mtype.chatFriend:socket.mtype.chatGroup),content:data.mine.content,toid:data.to.id});
+                        return;
+                    });
+                    //监听查看群员
+                    layim.on('members', function(data){
+                        //console.log(data);
+                    });
+                    //监听天窗口的切换
+                    layim.on('chatChange', function(res){
+                        var t = res.data.type=='friend';
+                        socket.send({
+                            mtype:t? socket.mtype.checkIsOnline:socket.mtype.checkOnlineCount,
+                            id:res.data.id
+                        });
+                    });
+                });
+            });
        },
        openDialog() {
            try {
