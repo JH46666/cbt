f9b3a764fff13f91f7cc9f0dba857a89d33c1535
caixinjia <526113132@qq.com>
2018-03-08 16:35:04 +0800

    bug

--- build_wap/config/index.js
+++ build_wap/config/index.js
@@ -31,7 +31,8 @@ module.exports = {
         }
       },
       '/erp': {
-        target: 'http://192.168.7.212:8080/', // 接口的域名
+          target: 'http://java.im.test.yipicha.com',
+        // target: 'http://192.168.7.212:8080/', // 接口的域名
         secure: false,
         changeOrigin: true,
         pathRewrite:{
