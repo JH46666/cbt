1f00809736fdb78da669b36e7b537ef5dfe14e6e
huangchunhong <1358191801@qq.com>
2018-03-26 19:58:25 +0800

    茶帮通bugg

--- src_wap/components/collection.vue
+++ src_wap/components/collection.vue
@@ -17,13 +17,28 @@
                     </div>
                     <div class="pro-txt">
                         <h4>{{item.proName}}</h4>
-                        <p class="attr-txt">{{item.subTitle}}</p>
+                        <div class="product-price">
+                            <template  v-if="!$tool.isLogin()">
+                                <div class="off_shelf_tips">
+                                    ￥{{item.hidePriceGroups}}
+                                </div>
+                                <div class="detail_suggest_price">￥{{ item.marketPrice | toFix2 }}</div>
+                            </template>
+                            <template v-else>
+                                <div class="detail_now_price">
+                                    ￥{{item.priceGroups | toFix2  }}
+                                </div>
+                                <div class="detail_suggest_price">￥{{ item.marketPrice | toFix2 }}</div>
+                            </template>
+                        </div>
+                            
+                        <!-- <p class="attr-txt">{{item.subTitle}}</p>
                         <p class="price-box">
                             <span class="pro-price" v-if="!$tool.isLogin()">询价</span>
-                            <span class="pro-price" v-else>￥{{item.proPrice}}</span>
+                            <span class="pro-price" v-else>￥{{item.proPrice}}</span> -->
                             <!-- 其他的类型加other  class就行   ['茶厂','合作社','茶企','批发商','自营','自营']-->
-                            <span class="type" :class="{'other':item.tagNum <= 4}">{{bussinessType[item.tagNum-1]}}</span>
-                        </p>
+                            <!-- <span class="type" :class="{'other':item.tagNum <= 4}">{{bussinessType[item.tagNum-1]}}</span>
+                        </p> -->
                     </div>
                 </router-link>
             </div>
@@ -60,4 +75,18 @@
     }
 </script>
 
+<style>
+ /* .detail_now_price{
+    color:#f08200;
+    line-height: 1;
+    font-size: 0.4rem;
+    margin-right: 0.1rem;
+}
+.detail_suggest_price{
+    color:#999;
+    line-height: 1;
+    font-size: 0.3rem;
+    text-decoration: line-through;
+} */
+</style>
 
