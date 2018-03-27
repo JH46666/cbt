3dae7b96ddb4916cf1d150eceda12c6ee039c519
tangzhiming <tangzhiming@zuipin.cn>
2018-03-27 15:37:32 +0800

    3722

--- src_wap/views/center/index.vue
+++ src_wap/views/center/index.vue
@@ -61,9 +61,9 @@
                     <p>待付款</p>
                 </router-link>
                 <router-link :to="{name: '订单列表',query: {orderStatus:'groupping'}}" class="item">
-                    <mt-badge size="small" :class="{one:count.waitSend < 10}" v-if="count.waitSend > 0" type="error">{{ count.grouppingNum | ninenineAdd }}</mt-badge>
+                    <mt-badge size="small" :class="{one:count.waitSend < 10}" v-if="count.grouppingNum > 0" type="error">{{ count.grouppingNum | ninenineAdd }}</mt-badge>
                     <span><i class="icon-gerenzhongxin_pintuanzhong"></i></span>
-                    <p>待成团</p>
+                    <p>拼团中</p>
                 </router-link>
                 <router-link :to="{name: '订单列表',query: {orderStatus:'waitSend'}}" class="item">
                     <mt-badge size="small" type="error" :class="{one:count.waitSend < 10}" v-if="count.waitSend > 0">{{ count.waitSend | ninenineAdd }}</mt-badge>
