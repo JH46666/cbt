45d99361d87f88d0b2e1296ada014c1fa1f783d5
tangzhiming <tangzhiming@zuipin.cn>
2018-03-28 09:25:51 +0800

    商品管理

--- src_wap/views/seller/goodsManage.vue
+++ src_wap/views/seller/goodsManage.vue
@@ -57,8 +57,8 @@
                     <!-- 头部 caption -->
                     <div class="item-caption flex">
                         <div class="cap-l flex flex-1 align_items_c">
-                            <!-- <label class="check-cir" :class="{'checked':item.checked}" @click="item.checked = !item.checked"></label>
-                            <span>上架 {{item.createTime}}</span> -->
+                            <!-- <label class="check-cir" :class="{'checked':item.checked}" @click="item.checked = !item.checked"></label> -->
+                            <span>上架 {{item.createTime}}</span>
                         </div>
                         <div class="cap-r algin_r">
                             <span class="saled">已售<span class="number" v-if="item.salesNum"> {{item.salesNum}} </span><span class="number" v-else> 0 </span></span>
@@ -106,7 +106,7 @@
                     <!-- 头部 caption -->
                     <div class="item-caption flex">
                         <div class="cap-l flex flex-1 align_items_c">
-                            <label class="check-cir" :class="{'checked':item.checked}" @click="item.checked = !item.checked"></label>
+                            <!-- <label class="check-cir" :class="{'checked':item.checked}" @click="item.checked = !item.checked"></label> -->
                             <span>创建 {{item.createTime}}</span>
                         </div>
                         <div class="cap-r algin_r">
