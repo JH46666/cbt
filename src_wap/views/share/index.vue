f6b83714d6d86cc02269b72ed3a27cecc68d0b16
cxj <526113132@qq.com>
2018-04-08 10:40:11 +0800

    bug

--- src_wap/views/share/index.vue
+++ src_wap/views/share/index.vue
@@ -187,7 +187,8 @@
 </template>
 <script>
 import store from 'store';
-import wx from 'weixin-js-sdk';
+// import wx from 'weixin-js-sdk';
+let wx = require('weixin-js-sdk');
 export default {
   data(){
     return{
@@ -452,7 +453,7 @@ export default {
    },
    setWxShare(){
        wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.wxConfig.appId, // 必填，公众号的唯一标识
           timestamp: this.wxConfig.timestamp,  // 必填，生成签名的时间戳
           nonceStr: this.wxConfig.nonceStr,  // 必填，生成签名的随机串
