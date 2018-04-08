09391e8b0f8eb18e6977d5067fa231786b4acff0
cxj <526113132@qq.com>
2018-04-08 15:47:14 +0800

    bug

--- src_wap/views/share/index.vue
+++ src_wap/views/share/index.vue
@@ -449,8 +449,15 @@ export default {
        })
    },
    getWxConfig(){
+    let  url = ''
+    // 判断是否是ios微信浏览器
+    if (window.__wxjs_is_wkwebview === true) {
+     url = this.$store.state.url.split('#')[0]
+    } else {
+     url = window.location.href.split('#')[0]
+    }
      let data = {
-       'url':window.location.href.split('&')[0]
+       'url':url
      }
      return new Promise((resolve,reject)=>{
        this.$api.get('/wap/wechatShareConfig',data,res=>{
@@ -460,7 +467,7 @@ export default {
    },
    setWxShare(){
        wx.config({
-          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.wxConfig.appId, // 必填，公众号的唯一标识
           timestamp: this.wxConfig.timestamp,  // 必填，生成签名的时间戳
           nonceStr: this.wxConfig.nonceStr,  // 必填，生成签名的随机串
