3b9f26c8a4c46ace05e80239b555938fbb0c193a
cxj <526113132@qq.com>
2018-04-08 13:56:54 +0800

    bug

--- src_wap/views/share/index.vue
+++ src_wap/views/share/index.vue
@@ -234,10 +234,7 @@ export default {
     if(this.$route.query.open){
       this.shareDialogFlag = true
     }
-    this.getWxConfig().then((res)=>{
-      this.wxConfig = res.data;
-      this.setWxShare();
-    })
+
     // 获取团购信息
     this.searchGroup().then((res) =>{
       this.groupData = res.data;
@@ -280,6 +277,12 @@ export default {
           this.orderNo = res.data.mainOrder.mainrNo;
       });
     }
+  },
+  mounted(){
+    this.getWxConfig().then((res)=>{
+      this.wxConfig = res.data;
+      this.setWxShare();
+    })
   },
   methods: {
     searchGroup(){
@@ -453,7 +456,7 @@ export default {
    },
    setWxShare(){
        wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: this.wxConfig.appId, // 必填，公众号的唯一标识
           timestamp: this.wxConfig.timestamp,  // 必填，生成签名的时间戳
           nonceStr: this.wxConfig.nonceStr,  // 必填，生成签名的随机串
